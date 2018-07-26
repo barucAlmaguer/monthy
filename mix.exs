41ea778df20651976a97e93597ce36ebdf0ad51d
Adrian Rangel <adrian.rangel@gmail.com>
2018-07-26 15:19:52 -0500

    force elixir 1.7+

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ValiotApp.Mixfile do
     [
       app: :valiot_app,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
