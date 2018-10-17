27b6cb9f0767e08741d4df908e96180c67c2da14
Adrian Rangel <adrian.rangel@gmail.com>
2018-10-17 17:19:29 -0500

    set token_helper

--- config/test.exs
+++ config/test.exs
@@ -19,8 +19,9 @@ config :valiot_app, ValiotApp.Repo,
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :valiot_app, :token_helper, %{
-  url: "url para valiot auth ",
+  url: "https://test.valiot.app/",
   header: %{
-    authorization: "token"
+    authorization:
+      "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ2YWxpb3RfYXBwIiwiZXhwIjoxNTQyMjMzNTc2LCJpYXQiOjE1Mzk4MTQzNzYsImlzcyI6InZhbGlvdF9hcHAiLCJqdGkiOiI5ZTUyZDI1MC1kZTU3LTQ0ZTQtOTMwZi03NjYyOGFkOWYzYjYiLCJuYmYiOjE1Mzk4MTQzNzUsInN1YiI6IiIsInR5cCI6ImFjY2VzcyJ9.CFFFFUsfmWBHX_B0GLlq9BASg91UkXcPPUCVXdXbX9nB9LzwfeH1Pejs7NX0gSCRkC8DR-1j3BL__8bGhjZkiw"
   }
 }
