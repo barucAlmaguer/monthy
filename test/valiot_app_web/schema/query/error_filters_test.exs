223dfed6f7165759e3ac71bc96b123ef62727d11
Adrian Rangel <adrian.rangel@gmail.com>
2018-10-17 17:49:26 -0500

    fix tests

--- test/valiot_app_web/schema/query/error_filters_test.exs
+++ test/valiot_app_web/schema/query/error_filters_test.exs
@@ -2,7 +2,7 @@ defmodule ValiotApp.Schema.Query.FiltersTests do
   use ValiotAppWeb.ConnCase, async: true
 
   # Change to your corresponding Authorization Bearer token:
-  @token "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ2YWxpb3RfYXBwIiwiZXhwIjoxNTMxNDk5NzE4LCJpYXQiOjE1MjkwODA1MTgsImlzcyI6InZhbGlvdF9hcHAiLCJqdGkiOiIyODY4NjVjZi1iN2UzLTQyMjctOWYzNS0xY2ViZGUwOWE1NTQiLCJuYmYiOjE1MjkwODA1MTcsInN1YiI6IjQiLCJ0eXAiOiJhY2Nlc3MifQ.I3PV52s4fdwtdSiIBS8irozcaTzCeFg3smtQp_E7apVvXK0_2Zh2mrtpDGuKt4VgVWmoPN4jlHHEgpUs--iHsw"
+  @token Application.get_env(:valiot_app, :token_helper).header.authorization
 
   # If you do not need a request header with a token feel free to erase them
 
--- test/valiot_app_web/schema/query/has_one_relationship_test.exs
+++ test/valiot_app_web/schema/query/has_one_relationship_test.exs
@@ -1,7 +1,7 @@
 defmodule ValiotApp.Schema.Query.HasOneRelationshipTest do
   use ValiotAppWeb.ConnCase, async: true
 
-  @token "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ2YWxpb3RfYXBwIiwiZXhwIjoxNTMxNDk5NzE4LCJpYXQiOjE1MjkwODA1MTgsImlzcyI6InZhbGlvdF9hcHAiLCJqdGkiOiIyODY4NjVjZi1iN2UzLTQyMjctOWYzNS0xY2ViZGUwOWE1NTQiLCJuYmYiOjE1MjkwODA1MTcsInN1YiI6IjQiLCJ0eXAiOiJhY2Nlc3MifQ.I3PV52s4fdwtdSiIBS8irozcaTzCeFg3smtQp_E7apVvXK0_2Zh2mrtpDGuKt4VgVWmoPN4jlHHEgpUs--iHsw"
+  @token Application.get_env(:valiot_app, :token_helper).header.authorization
 
   setup do
     Code.eval_string(
